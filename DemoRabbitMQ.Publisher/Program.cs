19d0f6382f92b6c590a6a39269ca518eee73c196
Tanju BOZOK <tanjubozok@hotmail.com>
2023-03-07 14:16:13 +0300

    Mesaj gonderme ve okuma işlemi tamamlandı.

--- DemoRabbitMQ.Publisher/Program.cs
+++ DemoRabbitMQ.Publisher/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory connectionFactory = new()
+{
+    Uri = new Uri("amqps://kdlvxpkh:Nx1oLT7msL9WqSA-L2TVoS_ry8wSbFlv@whale.rmq.cloudamqp.com/kdlvxpkh")
+};
+
+using var connection = connectionFactory.CreateConnection();
+var channel = connection.CreateModel();
+channel.QueueDeclare("hello-queue", true, false, false);
+
+string mesage = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+var messageBody = Encoding.UTF8.GetBytes(mesage);
+
+channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+Console.WriteLine("Mesaj gönderildi");
+Console.ReadLine();
--- DemoRabbitMQ.Subscriber/Program.cs
+++ DemoRabbitMQ.Subscriber/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory connectionFactory = new()
+{
+    Uri = new Uri("amqps://kdlvxpkh:Nx1oLT7msL9WqSA-L2TVoS_ry8wSbFlv@whale.rmq.cloudamqp.com/kdlvxpkh")
+};
+
+using var connection = connectionFactory.CreateConnection();
+var channel = connection.CreateModel();
+channel.QueueDeclare("hello-queue", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume("hello-queue", true, consumer);
+
+consumer.Received += Consumer_Received;
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine($"Gelen message : {message}");
+}
+
+Console.WriteLine("Mesaj okuma bitti...");
+Console.ReadLine();
